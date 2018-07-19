b687e8fb6df4c4638f3499663772b5032d882f2e
liangchengcheng <1038127753@qq.com>
2018-07-19 18:08:31 +0800

    add menu file
    add menu file

---
+++ Gaoxi-Common-Service-Facade/src/main/java/com/gaoxi/enumeration/user/UserStateEnum.java
@@ -0,0 +1,34 @@
+package com.gaoxi.enumeration.user;
+
+import com.gaoxi.enumeration.BaseEnum;
+
+public enum UserStateEnum implements BaseEnum {
+    ON(1, "启用"),
+    OFF(0, "禁用");
+
+    private int code;
+    private String msg;
+
+    UserStateEnum(int code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    @Override
+    public int getCode() {
+        return code;
+    }
+
+    @Override
+    public String getMsg() {
+        return msg;
+    }
+
+    @Override
+    public String toString() {
+        return "UserStateEnum{" +
+                "code=" + code +
+                ", msg='" + msg + '\'' +
+                '}';
+    }
+}
---
+++ Gaoxi-Common-Service-Facade/src/main/java/com/gaoxi/enumeration/user/UserTypeEnum.java
@@ -0,0 +1,41 @@
+package com.gaoxi.enumeration.user;
+
+import com.gaoxi.enumeration.BaseEnum;
+
+public enum UserTypeEnum implements BaseEnum {
+    Person(1,"个人用户"),
+    Company(2,"企业用户"),
+    ADMIN(3,"管理员");
+
+    private int code;
+    private String msg;
+
+    UserTypeEnum(int code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    /**
+     * 根据code获取枚举
+     */
+    public static UserTypeEnum getEnumByCode(int code) {
+        for (UserTypeEnum userTypeEnum : UserTypeEnum.values()) {
+            if (userTypeEnum.getCode() == code) {
+                return userTypeEnum;
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public int getCode() {
+        return code;
+    }
+
+    @Override
+    public String getMsg() {
+        return msg;
+    }
+
+}
